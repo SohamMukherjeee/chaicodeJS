5bea4b11b9022aafb84add324750b28927db405f
Soham Mukherjee <myselfmukherjeesoham@gmail.com>
2024-07-14 18:22:56 +0530

    day 1

---
+++ Variables&DataType.js
@@ -0,0 +1,19 @@
+//  Task 1
+var a=23;
+console.log(a);
+
+
+// Task 2
+let b="Soham is good boy";
+console.log(b);
+
+// Task 3
+const c=true;
+console.log(c);
+
+// Task 4
+console.log("a-->"+typeof(a)+" b-->"+typeof(b)+"  c-->"+typeof(c));
+
+// Task 5
+c=false;
+console.log(c);
